b536a2bf74a5e2f5b0f5198fb4bc8f8677b953dc
Vish-afk <jayavishnu404@gmail.com>
2025-05-13 00:40:03 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,54 +4,63 @@ import Overview from "../Overview/Overview";
 import Transaction from "../Transaction/Transaction";
 
 const Container = styled.div`
-    background-color: white;
-    color: #0d1d2c;
-    display: flex;
-    flex-direction: column;
-    padding: 10px 22px;
-    font-size: 18px;
-    width: 360px;
-    align-items: center;
-    justify-content: space-between;
-
+  background-color: white;
+  color: #0d1d2c;
+  display: flex;
+  flex-direction: column;
+  padding: 10px 22px;
+  font-size: 18px;
+  width: 360px;
+  align-items: center;
+  justify-content: space-between;
 `;
 
-const Home = (props) => {
-    const [transactions, updateTransaction] = useState([]);
-    const [expense, updateExpense] = useState(0);
-    const [income, updateIncome] = useState(0);
+const Home = () => {
+  const [transactions, updateTransaction] = useState([]);
+  const [expense, updateExpense] = useState(0);
+  const [income, updateIncome] = useState(0);
+  const [monthlyLimit, setMonthlyLimit] = useState(0);
 
-    const calculateBalance = () => {
-        let exp = 0;
-        let inc = 0;
-        transactions.map((payload) =>
-            payload.type === "EXPENSE"
-                ? (exp = exp + payload.amount)
-                : (inc = inc + payload.amount),
-        );
-        updateExpense(exp);
-        updateIncome(inc);
-    };
-    useEffect(() => calculateBalance(), [transactions]);
-
-    const addTransaction = (payload) => {
-        const transactionArray = [...transactions];
-        transactionArray.push(payload);
-        updateTransaction(transactionArray);
-    };
-    return (
-        <Container>
-            <Overview
-                expense={expense}
-                income={income}
-                addTransaction={addTransaction}
-            />
-            {transactions?.length ? (
-                <Transaction transactions={transactions} />
-            ) : (
-                ""
-            )}
-        </Container>
+  const calculateBalance = () => {
+    let exp = 0, inc = 0;
+    transactions.forEach((payload) =>
+      payload.type === "EXPENSE"
+        ? (exp += payload.amount)
+        : (inc += payload.amount)
     );
+    updateExpense(exp);
+    updateIncome(inc);
+  };
+
+  useEffect(() => {
+    calculateBalance();
+  }, [transactions]);
+
+  const addTransaction = (payload) => {
+    updateTransaction([...transactions, payload]);
+  };
+
+  const deleteTransaction = (id) => {
+    updateTransaction(transactions.filter((txn) => txn.id !== id));
+  };
+
+  return (
+    <Container>
+      <Overview
+        expense={expense}
+        income={income}
+        addTransaction={addTransaction}
+        monthlyLimit={monthlyLimit}
+        setMonthlyLimit={setMonthlyLimit}
+      />
+      {transactions.length > 0 && (
+        <Transaction
+          transactions={transactions}
+          deleteTransaction={deleteTransaction}
+        />
+      )}
+    </Container>
+  );
 };
+
 export default Home;
